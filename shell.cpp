1b3c76f3e68a735f612a0c2b67aa59b16237321e
Kathia Barahona <katbara03@gmail.com>
2016-09-07 19:04:33 -0400

    shell KBara

---
+++ shell.cpp
@@ -0,0 +1,238 @@
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <iostream>
+#include <ctype.h>
+#include <cstring>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sstream>
+#include <fstream>
+#include <string>
+#define token_buffer_size 64
+#define token_delim " \t\r\n\a"
+
+using namespace std;
+void shell_loop();
+char **shell_split_line(char*);
+int shell_execute(char **);
+int shell_launch(char **);
+int shell_num_builtins();
+int shell_ls(char**);
+int shell_cd(char**);
+int shell_mkdir(char**);
+int shell_chmod(char**);
+int shell_rmdir(char**);
+int shell_rm(char**);
+int shell_cat(char**);
+int shell_ln(char**);
+int shell_ps(char**);
+int shell_kill(char**);
+int shell_help(char**);
+int shell_exit(char**);
+
+char const *builtin_str[] = {"ls","cd","mkdir","chmod","rmdir","rm","cat","ln","ps","help","exit"};
+int (*builtin_func[])(char**) = {
+    &shell_ls,
+    &shell_cd,
+    &shell_mkdir,
+    &shell_chmod,
+    &shell_rmdir,
+    &shell_rm,
+    &shell_cat,
+    &shell_ln,
+    &shell_ps,
+    &shell_help,
+    &shell_exit
+};
+
+int main(int argc, char const *argv[])
+{
+    shell_loop();
+    return EXIT_SUCCESS;
+}
+void shell_loop(){
+    char *line;
+    size_t buffer_size;
+    char **tokens;
+    int status;
+    do {
+        cout << "mi_sh> ";
+        line = NULL;
+        buffer_size = 0;
+        getline(&line, &buffer_size, stdin);//obtiene la linea de comando
+        tokens = shell_split_line(line);
+        status = shell_execute(tokens);
+
+        free(line);
+        free(tokens);
+    }while(status);
+}  
+char ** shell_split_line(char* line){
+    int buffer_size = token_buffer_size;
+    int t_position = 0;
+    char **tokens = (char **) malloc(buffer_size * sizeof(char *));
+    char *token;
+    if(!tokens){
+        cout << "Error de asignacion de memoria" << endl;
+        exit(EXIT_FAILURE);
+    }
+    token = strtok(line,token_delim);
+    while(token != NULL){
+        tokens[t_position] = token;
+        t_position++;
+        if(t_position >= buffer_size){
+            buffer_size += token_buffer_size;
+            tokens = (char **) realloc(NULL,buffer_size * sizeof(char *));
+            if(!tokens){
+                cout << "Error de asignacion de memoria" << endl;
+                exit(EXIT_FAILURE);
+            }
+
+        }
+        token = strtok(NULL,token_delim);
+    }
+    tokens[t_position] = NULL;
+    return tokens;
+}
+int shell_launch(char** tokens){
+    pid_t pid, wpid;
+    int status;
+    pid = fork();
+    if(pid == 0){
+        if(execvp(tokens[0],tokens) == -1){
+            perror("mi_sh");
+        }
+        exit(EXIT_FAILURE);
+    } else if(pid < 0){
+        perror("mi_sh");
+    }else {
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        }while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return 1;
+}
+int shell_execute(char** tokens){
+    if(tokens[0] == NULL){
+        return 1;
+    }
+    for (int i = 0; i < shell_num_builtins(); i++) {
+        if (strcmp(tokens[0], builtin_str[i]) == 0) {
+          return (*builtin_func[i])(tokens);
+        }
+      }
+
+  return shell_launch(tokens);
+}
+int shell_num_builtins(){
+    return sizeof(builtin_str) / sizeof(char *);
+}
+int shell_ls(char** tokens){
+
+
+    char cwd[1024];
+    DIR *mydir;
+    struct dirent *myfile;
+    struct stat mystat;
+   if (getcwd(cwd, sizeof(cwd)) != NULL){
+       fprintf(stdout, "Current working dir: %s\n", cwd);
+       mydir = opendir(cwd);
+        while((myfile = readdir(mydir)) != NULL)
+        {
+            
+            cout <<  myfile->d_name << endl;  
+            
+        }
+        closedir(mydir);
+
+   }else
+       perror("mi_sh");
+
+    return 1;
+}
+int shell_cd(char** tokens){
+    if(tokens[1] == NULL){
+        cout << "mi_sh: argumento esperado para \"cd\"" << endl;
+    }else{
+        if(chdir(tokens[1]) != 0){
+            perror("mi_sh");
+        }
+    }
+    return 1;
+}   
+int shell_mkdir(char** tokens){
+
+    if(tokens[1] == NULL){
+        cout << "mi_sh: argumento esperado para \"mkdir\"" << endl;
+    }else{
+        if(mkdir(tokens[1], S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) != 0){
+            perror("mi_sh");
+        }
+    }
+    return 1;
+}
+int shell_chmod(char** tokens){
+    if(tokens[1] == NULL){
+        cout << "mi_sh: argumento esperado para \"chmod\"" << endl;
+    }else{
+        if(tokens[2] == NULL){
+            cout << "mi_sh: argumento esperado para \"chmod\"" << endl;
+        }else{
+            int mode = strtol(tokens[1],0,8);
+
+            if(chmod(tokens[2],mode) != 0){
+                perror("mi_sh");
+            }
+        }
+    }
+    return 1;
+
+}
+int shell_rmdir(char** tokens){
+    
+    if(tokens[1] == NULL){
+        cout << "mi_sh: argumento esperado para \"rmdir\"" << endl;
+    }else{
+        if(rmdir(tokens[1]) != 0){
+            perror("mi_sh");
+        }
+    }
+    return 1;
+
+}
+int shell_rm(char** tokens){
+    // if(tokens[1] == NULL){
+    //     cout << "mi_sh: argumento esperado para \"rm\"" << endl;
+    // }else{
+    //     if(rm(tokens[1]) != 0){
+    //         perror("mi_sh");
+    //     }
+    // }
+    return 1;
+}
+int shell_cat(char** tokens){
+
+}
+int shell_ln(char** tokens){
+
+}
+int shell_ps(char** tokens){
+
+}
+int shell_help(char** tokens){
+    int i;
+    cout << "Proyecto 2, Sistemas Operativos I" << endl;
+    cout << "Grupo: Fernando Reyes, Kathia Barahona" << endl;
+    cout << "Puede utilizar los siguientes comandos: " <<endl;
+    for(i = 0 ; i < shell_num_builtins(); i++){
+        cout << builtin_str[i] << endl;
+    }
+    return 1;
+
+}
+int shell_exit(char** tokens){
+    return 0 ;
+}
